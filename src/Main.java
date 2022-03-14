e2597893748a6158ebe9648423a1bbcea18524c1
Sergi García <kazutoxyt@gmail.com>
2022-03-14 20:19:28 +0100

    Pantalla completa
    He añadido la pantalla completa en el menú principal del programa para que se vea mucho mejor

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ import javax.swing.JFrame;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.ImageIcon;
+import java.awt.Color;
 
 public class Main {
 
@@ -36,17 +38,23 @@ public class Main {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		Ventana ventana1 = new Ventana();
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
+		ventana1.fullScreen(frame);
 		
-		JButton btnNewButton = new JButton("Bot\u00F3n de prueba");
-		btnNewButton.addActionListener(new ActionListener() {
+		JButton exit_btn = new JButton("");
+		exit_btn.setBackground(Color.WHITE);
+		exit_btn.setForeground(Color.WHITE);
+		exit_btn.setIcon(new ImageIcon("C:\\Users\\sergi\\Desktop\\Proyecto DAM\\Proyecto-DAM\\src\\img\\exit_btn.png"));
+		exit_btn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				
+				ventana1.exit();
+				
 			}
 		});
-		btnNewButton.setBounds(162, 217, 113, 33);
-		frame.getContentPane().add(btnNewButton);
+		frame.getContentPane().setLayout(null);
+		exit_btn.setBounds(1785, 933, 109, 97);
+		frame.getContentPane().add(exit_btn);
 	}
 }
---
+++ src/Ventana.java
@@ -0,0 +1,35 @@
+import java.awt.Dimension;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+
+import javax.swing.JFrame;
+
+public class Ventana {
+private GraphicsEnvironment ge =GraphicsEnvironment.getLocalGraphicsEnvironment();
+private GraphicsDevice gd = null;
+
+public void fullScreen(JFrame frame) {
+gd = ge.getDefaultScreenDevice();
+// Si no hay soporte adecuado lo mostramos por pantalla
+if (!gd.isFullScreenSupported()) {
+System.out.println("WARNING: No hay soporte.\n");
+} else {
+System.out.println("INFO: Detectado soporte\n");
+}
+try {
+// Activamos el modo a pantalla completa
+gd.setFullScreenWindow(frame);
+// Inicializamos toda la interfaz gráfica                    
+} catch (Throwable e) {
+e.printStackTrace();
+System.out.println("ERROR CRITICO: " + e.getMessage());
+}
+}
+
+public void exit() {
+//   salimos del modo a pantalla completa
+gd.setFullScreenWindow(null);
+//   salimos de la aplicación
+System.exit(0);
+}
+}
